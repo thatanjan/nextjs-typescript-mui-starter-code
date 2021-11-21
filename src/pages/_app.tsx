020ee44840cd221aa42444057b0be9576c33ad4c
thatanjan <thatanjan@gmail.com>
2021-11-21 11:44:19 +0600

    starter code added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import createEmotionCache from 'utils/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+	return (
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<title>My page</title>
+				<meta name='viewport' content='initial-scale=1, width=device-width' />
+			</Head>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</CacheProvider>
+	)
+}
